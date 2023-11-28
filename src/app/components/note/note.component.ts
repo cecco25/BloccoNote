c103a09b8ed5169ba599e7268da8bb854f2c0fb7
CeccoBolt <cecchinimichael.code@gmail.com>
2023-11-28 15:24:35 +0100

    Modifiche Stile Nota ed Home

--- src/app/components/note/note.component.ts
+++ src/app/components/note/note.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges, OnDestroy, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, Output, OnChanges, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-note', //Il selector identifica il tag html che dovete richiamare
