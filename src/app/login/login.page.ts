4dde076395af48c19c4e1c72761404126480f62e
CeccoBolt <cecchinimichael.code@gmail.com>
2023-12-15 23:57:07 +0100

    Fix Login
    Co-Authored-By: Francesco Pauselli <81373986+francescopausellii@users.noreply.github.com>

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../services/user.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -8,9 +10,28 @@ import { UserService } from '../services/user.service';
 })
 export class LoginPage implements OnInit {
 
-  constructor(private user: UserService) { }
+  email = "";
+  password = "";
+  apiUrl = 'http://www.seagullcode.it/ntapi';
+  headers = new HttpHeaders();
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+  }
+
+  login() {
+    this.headers.append('email', this.email);
+    this.headers.append('password', this.password);
+    console.log(this.email + " " + this.password)
+    this.http.post<any>(`${this.apiUrl}/auth`, null, { headers: this.headers })
+      .pipe(map((res: any) => {
+        if (res && res.session) {
+          console.log(res.session);
+        }
+        return res;
+      })
+      );
   }
 
 }
